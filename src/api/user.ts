94c4363ab0c546946562b0a6f662b0debf097522
lipeng <85272827+roc80@users.noreply.github.com>
2025-06-07 20:23:53 +0800

    dev 首页 新增推荐用户展示

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,5 +1,6 @@
 export const UserAPI = {
-    recommend: '/user/recommend',
+    getAllUsers: '/user/search/all',
+    recommendUsers: '/user/recommend',
     searchByTags: '/user/search/tags',
     current: '/user/current',
     logout: '/user/logout',
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,31 +1,90 @@
 <script setup lang="ts">
-import {onMounted, ref} from "vue";
+import {onMounted, ref, watch} from "vue";
 import myAxios from "@/utils/myAxios";
 import UserCardList from "@/components/UserCardList.vue";
 import {UserAPI} from "@/api/user";
 import {CommonResponse, User} from "@/typing";
 
-let userList = ref<User[]>([])
+let allUserList = ref<User[]>([])
+let recommendUserList = ref<User[]>([])
 // TODO@lp 切换页码，重新请求
 let pageNum = ref<number>(1)
 const pageSize = ref<number>(20)
-onMounted(async () => {
+
+const activeTabIndex = ref<number>(0)
+const hasLoadAll = ref<boolean>(false)
+const hasLoadRecommend = ref<boolean>(false)
+const isRecommendUserLoading = ref<boolean>(false)
+const isAllUserLoading = ref<boolean>(false)
+
+const fetchAllUsers = async () => {
+  if (hasLoadAll.value) return
   try {
-    const response = await myAxios.get<CommonResponse<User[]>>(UserAPI.recommend, {
+    const response = await myAxios.get<CommonResponse<User[]>>(UserAPI.getAllUsers, {
       params: {
         pageNum: pageNum.value,
         pageSize: pageSize.value,
       }
     })
-    userList.value = response.data ?? []
+    allUserList.value = response.data ?? []
+    hasLoadAll.value = true
   } catch (error) {
-    console.error(`首页数据加载失败 ${UserAPI.recommend}`, error);
+    console.error(`首页数据加载失败 ${UserAPI.getAllUsers}`, error);
+  }
+}
+
+const fetchRecommendUsers = async () => {
+  if (hasLoadRecommend.value) return
+  try {
+    const response = await myAxios.get<CommonResponse<User[]>>(UserAPI.recommendUsers, {
+      params: {
+        pageNum: pageNum.value,
+        pageSize: pageSize.value,
+      }
+    })
+    recommendUserList.value = response.data ?? []
+    hasLoadRecommend.value = true
+  } catch (error) {
+    console.error(`首页数据加载失败 ${UserAPI.recommendUsers}`, error);
+  }
+}
+
+onMounted(async () => {
+  await fetchRecommendUsers()
+})
+
+watch(activeTabIndex, async (newIndex, _) => {
+  if (newIndex === 0) {
+    await fetchRecommendUsers()
+  } else if (newIndex === 1) {
+    await fetchAllUsers()
   }
 })
 </script>
 
 <template>
-  <UserCardList :userList="userList"/>
+  <van-tabs v-model:active="activeTabIndex">
+    <van-tab title="推荐用户">
+      <van-pull-refresh v-model:loading="isRecommendUserLoading" @refresh="async () => {
+        isRecommendUserLoading = true;
+        hasLoadRecommend = false;
+        await fetchRecommendUsers();
+        isRecommendUserLoading = false;
+      }">
+        <UserCardList :userList="recommendUserList"/>
+      </van-pull-refresh>
+    </van-tab>
+    <van-tab title="所有用户">
+      <van-pull-refresh v-model:loading="isAllUserLoading" @refresh="async () => {
+        isAllUserLoading = true;
+        hasLoadAll = false;
+        await fetchAllUsers();
+        isAllUserLoading = false;
+      }">
+        <UserCardList :userList="allUserList"/>
+      </van-pull-refresh>
+    </van-tab>
+  </van-tabs>
 </template>
 
 <style scoped>
